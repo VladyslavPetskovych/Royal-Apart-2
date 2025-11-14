d38e5cf68d8c4b6507487c10dc3bc53e899ba650
vladyslav <peckovychv@gmail.com>
2025-11-14 12:12:48 +0200

    fix

--- server/routes/analis.js
+++ server/routes/analis.js
@@ -1,50 +1,48 @@
-// // routes/wubook.js
-// const express = require("express");
-// const axios = require("axios");
-// const router = express.Router();
-// const path = require("path");
-// const XLSX = require("xlsx");
+// routes/wubook.js
+const express = require("express");
+const axios = require("axios");
+const router = express.Router();
 
 
-// router.get("/ping", (req, res) => {
-//   res.json({ ok: true, route: "analis works" });
-// });
+router.get("/ping", (req, res) => {
+  res.json({ ok: true, route: "analis works" });
+});
 
-// router.post("/prices", async (req, res) => {
-//   const { lcode, pid, globalId, dfrom, dto } = req.body; // ✅ тут змінили
+router.post("/prices", async (req, res) => {
+  const { lcode, pid, globalId, dfrom, dto } = req.body; // ✅ тут змінили
 
-//   const token = "wr_9fd536d9-2894-441a-85eb-4b1a670e2ff2";
+  const token = "wr_9fd536d9-2894-441a-85eb-4b1a670e2ff2";
 
-//   const xml = `<?xml version="1.0"?>
-//   <methodCall>
-//     <methodName>fetch_plan_prices</methodName>
-//     <params>
-//       <param><value><string>${token}</string></value></param>
-//       <param><value><int>${lcode}</int></value></param>
-//       <param><value><int>${pid}</int></value></param>
-//       <param><value><string>${dfrom}</string></value></param>
-//       <param><value><string>${dto}</string></value></param>
-//       <param>
-//         <value>
-//           <array><data>
-//             <value><int>${globalId}</int></value>  
-//           </data></array>
-//         </value>
-//       </param>
-//     </params>
-//   </methodCall>`;
+  const xml = `<?xml version="1.0"?>
+  <methodCall>
+    <methodName>fetch_plan_prices</methodName>
+    <params>
+      <param><value><string>${token}</string></value></param>
+      <param><value><int>${lcode}</int></value></param>
+      <param><value><int>${pid}</int></value></param>
+      <param><value><string>${dfrom}</string></value></param>
+      <param><value><string>${dto}</string></value></param>
+      <param>
+        <value>
+          <array><data>
+            <value><int>${globalId}</int></value>  
+          </data></array>
+        </value>
+      </param>
+    </params>
+  </methodCall>`;
 
-//   try {
-//     const response = await axios.post("https://wired.wubook.net/xrws/", xml, {
-//       headers: { "Content-Type": "text/xml" },
-//     });
+  try {
+    const response = await axios.post("https://wired.wubook.net/xrws/", xml, {
+      headers: { "Content-Type": "text/xml" },
+    });
 
-//     res.send(response.data);
-//   } catch (err) {
-//     console.error("WuBook API Error:", err.message);
-//     res.status(500).json({ error: "WuBook API request failed" });
-//   }
-// });
+    res.send(response.data);
+  } catch (err) {
+    console.error("WuBook API Error:", err.message);
+    res.status(500).json({ error: "WuBook API request failed" });
+  }
+});
 
 // router.get("/data", (req, res) => {
 //   try {
@@ -68,4 +66,4 @@
 //   }
 // });
 
-// module.exports = router;
+module.exports = router;
